8df774b6412cf636a5738d87268303359ae6e0ff
sagar <7208sk@gmail.com>
2021-10-22 06:06:48 +0530

    Creating main master branch for HASH & BST

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashAndBST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Hello Welcome to Hash & BTS problem******");
+        }
+    }
+}
